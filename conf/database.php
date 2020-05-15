f432dc9fa9aea1592ba727be04115c192325c3a9
he <18235780760@163.com>
2020-05-15 16:59:45 +0800

    add project

---
+++ conf/database.php
@@ -0,0 +1,10 @@
+<?php
+  return array(
+	'dsn' => 'mysql:host=localhost;dbname=wind;port=3306',
+	'user' => 'root',
+	'pwd' => 'root',
+	'charset' => 'utf8',
+	'tableprefix' => 'pw_',
+	'engine' => 'MyISAM',
+);
+?>
---
+++ data/log/admin_log.php
@@ -0,0 +1,22 @@
+<?php die;?>
+|admin|Login Successful|127.0.0.1|1589531405|/admin.php?a=login|username=admin, password=***, submit=, csrf_token=4995d2b722793c2b|
+|admin|bbs/setforum/run|127.0.0.1|1589531439|/admin.php?m=bbs&c=setforum||
+|admin|bbs/setforum/run|127.0.0.1|1589531451|/admin.php?m=bbs&c=setforum||
+|admin|config/config/run|127.0.0.1|1589531466|/admin.php?m=config&c=config||
+|admin|bbs/configbbs/run|127.0.0.1|1589531496|/admin.php?m=bbs&c=configbbs||
+|admin|bbs/setforum/run|127.0.0.1|1589531514|/admin.php?m=bbs&c=setforum||
+|admin|bbs/setforum/edit|127.0.0.1|1589531521|/admin.php?fid=1&m=bbs&c=setforum&a=edit||
+|admin|Login Successful|127.0.0.1|1589531886|/admin.php?a=login|username=admin, password=***, submit=, csrf_token=3d76f92a67cf79d8|
+|admin|bbs/setforum/run|127.0.0.1|1589531898|/admin.php?m=bbs&c=setforum||
+|admin|bbs/setforum/edit|127.0.0.1|1589531903|/admin.php?fid=1&m=bbs&c=setforum&a=edit||
+|admin|bbs/setforum/doedit|127.0.0.1|1589531932|/admin.php?_json=1&m=bbs&c=setforum&a=doedit|fid=1, forumname=游戏, manager=admin, vieworder=0, descrip=游戏, style=, across=2, isshow=1, jumpurl=, icon=, logo=, forumdomain=, seo=array(title=, keywords=, description=)numofthreadtitle=50, threadperpage=20, newtime=60, threadorderby=0, password=***, csrf_token=3d76f92a67cf79d8|
+|admin|bbs/setforum/edit|127.0.0.1|1589531936|/admin.php?fid=1&m=bbs&c=setforum&a=edit&tab=nav_jb||
+|admin|bbs/setforum/run|127.0.0.1|1589531956|/admin.php?m=bbs&c=setforum||
+|admin|bbs/setforum/edit|127.0.0.1|1589531960|/admin.php?fid=2&m=bbs&c=setforum&a=edit||
+|admin|bbs/setforum/doedit|127.0.0.1|1589531973|/admin.php?_json=1&m=bbs&c=setforum&a=doedit|fid=2, forumname=dota2, manager=admin, vieworder=0, parentid=1, descrip=, style=, across=2, isshow=1, jumpurl=, icon=, logo=, forumdomain=, seo=array(title=, keywords=, description=)numofthreadtitle=50, threadperpage=20, readperpage=15, newtime=60, threadorderby=0, minlengthofcontent=3, locktime=, edittime=, allowtype=array(0=default)typeorder=array(default=0, poll=)allowhide=1, allowsell=1, contentcheck=0, ifthumb=0, thumbwidth=, thumbheight=, water=0, waterimg=mark.gif, topic_type=0, force_topic_type=0, topic_type_display=1, password=***, creditset=array(post_topic=array(limit=, credit=array(1=, 2=, 3=))delete_topic=array(limit=, credit=array(1=, 2=, 3=))post_reply=array(limit=, credit=array(1=, 2=, 3=))delete_reply=array(limit=, credit=array(1=, 2=, 3=))digest_topic=array(limit=, credit=array(1=, 2=, 3=))remove_digest=array(limit=, credit=array(1=, 2=, 3=))push_thread=array(limit=, credit=array(1=, 2=, 3=))upload_att=array(limit=, credit=array(1=, 2=, 3=))download_att=array(limit=, credit=array(1=, 2=, 3=))belike=array(limit=, credit=array(1=, 2=, 3=)))csrf_token=3d76f92a67cf79d8|
+|admin|bbs/setforum/edit|127.0.0.1|1589531977|/admin.php?fid=2&m=bbs&c=setforum&a=edit&tab=nav_jb||
+|admin|Login Successful|127.0.0.1|1589533055|/admin.php?a=login|username=admin, password=***, submit=, csrf_token=3d76f92a67cf79d8|
+|admin|u/manage/run|127.0.0.1|1589533075|/admin.php?m=u&c=manage||
+|admin|default/founder/run|127.0.0.1|1589533098|/admin.php?c=founder||
+|admin|default/auth/run|127.0.0.1|1589533102|/admin.php?c=auth||
+|admin|default/safe/run|127.0.0.1|1589533105|/admin.php?c=safe||
